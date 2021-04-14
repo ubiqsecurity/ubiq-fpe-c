a4229607898fc3715c2950e6202588d7e7e52a88
John Tyner <jtyner@ubiqsecurity.com>
2021-04-14 13:06:42 -0700

    consolidate ff1 encryption/decryption implementations

--- src/lib/ff1.c
+++ src/lib/ff1.c
@@ -70,10 +70,12 @@ int ff1_ciph(uint8_t * const dst,
     return ff1_prf(dst, K, k, src, 16);
 }
 
-int ff1_encrypt(char * const Y,
-                const uint8_t * const K, const size_t k,
-                const uint8_t * const T, const size_t t,
-                const char * const X, const unsigned int radix)
+static
+int ff1_cipher(char * const Y,
+               const uint8_t * const K, const size_t k,
+               const uint8_t * const T, const size_t t,
+               const char * const X, const unsigned int radix,
+               const int encrypt)
 {
     const unsigned int n = strlen(X);
     const unsigned int u = n / 2, v = n - u;
@@ -118,8 +120,13 @@ int ff1_encrypt(char * const Y,
     Q = P + p;
     R = Q + q;
 
-    memcpy(A, X + 0, u); A[u] = '\0';
-    memcpy(B, X + u, v); B[v] = '\0';
+    if (encrypt) {
+        memcpy(A, X + 0, u); A[u] = '\0';
+        memcpy(B, X + u, v); B[v] = '\0';
+    } else {
+        memcpy(B, X + 0, u); B[u] = '\0';
+        memcpy(A, X + u, v); A[v] = '\0';
+    }
 
     P[0] = 1;
     P[1] = 2;
@@ -133,7 +140,7 @@ int ff1_encrypt(char * const Y,
     *(uint32_t *)&P[12] = htonl(t);
 
     for (unsigned int i = 0; i < 10; i++) {
-        const unsigned int m = (i % 2) ? v : u;
+        const unsigned int m = ((i + encrypt) % 2) ? u : v;
 
         uint8_t * numb;
         size_t numc;
@@ -143,7 +150,7 @@ int ff1_encrypt(char * const Y,
 
         memcpy(Q, T, t);
         memset(Q + t, 0, z);
-        Q[t + z] = i;
+        Q[t + z] = encrypt ? i : (9 - i);
         if (b <= numc) {
             memcpy(&Q[t + z + 1], numb, b);
         } else {
@@ -166,7 +173,11 @@ int ff1_encrypt(char * const Y,
 
         bigint_import(&y, R, d);
         bigint_set_str(&c, A, radix);
-        bigint_add(&c, &c, &y);
+        if (encrypt) {
+            bigint_add(&c, &c, &y);
+        } else {
+            bigint_sub(&c, &c, &y);
+        }
         bigint_set_ui(&y, radix);
         bigint_pow_ui(&y, &y, m);
         bigint_mod(&c, &c, &y);
@@ -181,8 +192,13 @@ int ff1_encrypt(char * const Y,
         }
     }
 
-    strcpy(Y, A);
-    strcat(Y, B);
+    if (encrypt) {
+        strcpy(Y, A);
+        strcat(Y, B);
+    } else {
+        strcpy(Y, B);
+        strcat(Y, A);
+    }
 
     memset(scratch.buf, 0, scratch.len);
     free(scratch.buf);
@@ -193,125 +209,18 @@ int ff1_encrypt(char * const Y,
     return 0;
 }
 
+int ff1_encrypt(char * const Y,
+                const uint8_t * const K, const size_t k,
+                const uint8_t * const T, const size_t t,
+                const char * const X, const unsigned int radix)
+{
+    return ff1_cipher(Y, K, k, T, t, X, radix, 1);
+}
+
 int ff1_decrypt(char * const Y,
                 const uint8_t * const K, const size_t k,
                 const uint8_t * const T, const size_t t,
                 const char * const X, const unsigned int radix)
 {
-    const unsigned int n = strlen(X);
-    const unsigned int u = n / 2, v = n - u;
-
-    const unsigned int b = ((unsigned int)ceil(log2(radix) * v) + 7) / 8;
-    const unsigned int d = 4 * ((b + 3) / 4) + 4;
-
-    const unsigned int p = 16;
-    const unsigned int q = ((t + b + 1 + 15) / 16) * 16;
-    const unsigned int z = q - (t + b + 1);
-    const unsigned int r = 16;
-
-    struct {
-        void * buf;
-        size_t len;
-    } scratch;
-
-    char * A, * B, * C;
-    uint8_t * P, * Q, * R;
-
-    bigint_t y, c;
-
-    bigint_init(&y);
-    bigint_init(&c);
-
-    scratch.len = 3 * (v + 2) + p + q + r + ((((d + 15) / 16) - 1) * 16);
-    scratch.buf = malloc(scratch.len);
-    if (!scratch.buf) {
-        bigint_deinit(&c);
-        bigint_deinit(&y);
-        return -ENOMEM;
-    }
-
-    /*
-     * TODO: maybe P, Q, and R should be at the
-     * front so that they can be 16-byte aligned
-     */
-    A = scratch.buf;
-    B = A + v + 2;
-    C = B + v + 2;
-    P = C + v + 2;
-    Q = P + p;
-    R = Q + q;
-
-    memcpy(A, X + 0, u); A[u] = '\0';
-    memcpy(B, X + u, v); B[v] = '\0';
-
-    P[0] = 1;
-    P[1] = 2;
-    P[2] = 1;
-    P[3] = 0;
-    P[4] = radix >> 8;
-    P[5] = radix;
-    P[6] = 10;
-    P[7] = u;
-    *(uint32_t *)&P[8]  = htonl(n);
-    *(uint32_t *)&P[12] = htonl(t);
-
-    for (int i = 9; i >= 0; i--) {
-        const unsigned int m = (i % 2) ? v : u;
-
-        uint8_t * numb;
-        size_t numc;
-
-        bigint_set_str(&c, A, radix);
-        numb = bigint_export(&c, &numc);
-
-        memcpy(Q, T, t);
-        memset(Q + t, 0, z);
-        Q[t + z] = i;
-        if (b <= numc) {
-            memcpy(&Q[t + z + 1], numb, b);
-        } else {
-            memset(&Q[t + z + 1], 0, b - numc);
-            memcpy(&Q[t + z + 1 + (b - numc)], numb, numc);
-        }
-
-        free(numb);
-
-        ff1_prf(R, K, k, P, p + q);
-
-        for (unsigned int j = 1; j < (d + 15) / 16; j++) {
-            memset(&R[16 * j], 0, 16 - sizeof(j));
-            *(unsigned int *)&R[16 * j + 16 - sizeof(j)] = htonl(j);
-
-            memxor(&R[16 * j], &R[0], &R[16 * j], 16);
-
-            ff1_ciph(&R[16 * j], K, k, &R[16 * j]);
-        }
-
-        bigint_import(&y, R, d);
-        bigint_set_str(&c, B, radix);
-        bigint_sub(&c, &c, &y);
-        bigint_set_ui(&y, radix);
-        bigint_pow_ui(&y, &y, m);
-        bigint_mod(&c, &c, &y);
-
-        ffx_str(C, v + 2, m, radix, &c);
-
-        {
-            char * const tmp = B;
-            B = A;
-            A = C;
-            C = tmp;
-        }
-    }
-
-    strcpy(Y, A);
-    strcat(Y, B);
-
-    memset(scratch.buf, 0, scratch.len);
-    free(scratch.buf);
-
-    bigint_deinit(&c);
-    bigint_deinit(&y);
-
-    return 0;
+    return ff1_cipher(Y, K, k, T, t, X, radix, 0);
 }
