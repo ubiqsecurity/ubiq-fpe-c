a0ced30a4114e98fcdca74946f624005e16c4d1f
John Tyner <jtyner@ubiqsecurity.com>
2021-04-15 16:53:11 -0700

    document interfaces

--- src/include/ubiq/fpe/ff1.h
+++ src/include/ubiq/fpe/ff1.h
@@ -9,21 +9,89 @@ __BEGIN_DECLS
 
 struct ff1_ctx;
 
+/*
+ * Create a context structure for use with the FF1 algorithm
+ *
+ * The created structure can be used for encryption or decryption
+ * or both. However, the structure is not thread-safe. For example,
+ * it cannot be used for simultaneous encryptions/decryptions in
+ * multiple threads.
+ *
+ * @ctx: Pointer to location to store pointer to context data
+ *       Caller supplies the address to a pointer. This function
+ *       will fill it in
+ * @keybuf: Pointer to key data
+ * @keylen: Number of bytes in the key (must be 16, 24, 32)
+ * @twkbuf: Pointer to tweak data (may be NULL)
+ * @twklen: Number of bytes of tweak data (may be 0)
+ * @mintwklen: The minimum number of bytes allowed for the tweak data
+ * @maxtwklen: The maximum number of bytes allowed for the tweak data
+ *             This number may be 0 to indicate that the maximum is unlimited
+ * @radix: The radix of the plain/cipher text data
+ *
+ * @return 0 on success or a negative error number on failure
+ */
 int ff1_ctx_create(struct ff1_ctx ** const ctx,
                    const uint8_t * const keybuf, const size_t keylen,
                    const uint8_t * const twkbuf, const size_t twklen,
                    const size_t mintwklen, const size_t maxtwklen,
                    const unsigned int radix);
 
+/*
+ * Encrypt data using the FF1 algorithm
+ *
+ * Encryption is a "one-shot" operation. The entire plain text must
+ * be passed in, and the entire cipher text is returned. The cipher
+ * text uses the same "alphabet" as the plain text and will be the
+ * same length as the plain text.
+ *
+ * @ctx: The pointer returned by the create function
+ * @Y: A pointer to the location to output the cipher text. This space
+ *     is allocated by the caller and must be at least as long as the
+ *     plain text. The parameter must include space for a nul-terminator.
+ * @X: A pointer to the location of the plain text. The plain text must
+ *     be nul-terminated
+ * @T: A pointer to the "tweak" parameter. This is a sequence of bytes
+ *     and may be NULL. In the event that the tweak is NULL, the tweak
+ *     supplied to the create function will be used.
+ * @t: The number of bytes pointed to by @T. If T is NULL, t must be zero.
+ *
+ * @return 0 on success or a negative error number on failure
+ */
 int ff1_encrypt(struct ff1_ctx * const ctx,
                 char * const Y,
                 const char * const X,
                 const uint8_t * const T, const size_t t);
+/*
+ * Decrypt data using the FF1 algorithm
+ *
+ * Decryption is a "one-shot" operation. The entire cipher text must
+ * be passed in, and the entire plain text is returned.
+ *
+ * @ctx: The pointer returned by the create function
+ * @Y: A pointer to the location to output the plain text. This space
+ *     is allocated by the caller and must be at least as long as the
+ *     cipher text. The parameter must include space for a nul-terminator.
+ * @X: A pointer to the location of the cipher text. The cipher text must
+ *     be nul-terminated
+ * @T: A pointer to the "tweak" parameter. This is a sequence of bytes
+ *     and may be NULL. In the event that the tweak is NULL, the tweak
+ *     supplied to the create function will be used. The tweak must be
+ *     the same as that used to perform the encryption
+ * @t: The number of bytes pointed to by @T. If T is NULL, t must be zero.
+ *
+ * @return 0 on success or a negative error number on failure
+ */
 int ff1_decrypt(struct ff1_ctx * const ctx,
                 char * const Y,
                 const char * const X,
                 const uint8_t * const T, const size_t t);
 
+/*
+ * Destroy the context structure associated with the FF1 algorithm
+ *
+ * @ctx: The pointer returned by the create function
+ */
 void ff1_ctx_destroy(struct ff1_ctx * const ctx);
 
 __END_DECLS
--- src/include/ubiq/fpe/ff3_1.h
+++ src/include/ubiq/fpe/ff3_1.h
@@ -9,18 +9,81 @@ __BEGIN_DECLS
 
 struct ff3_1_ctx;
 
+/*
+ * Create a context structure for use with the FF3-1 algorithm
+ *
+ * The created structure can be used for encryption or decryption
+ * or both. However, the structure is not thread-safe. For example,
+ * it cannot be used for simultaneous encryptions/decryptions in
+ * multiple threads.
+ *
+ * @ctx: Pointer to location to store pointer to context data
+ *       Caller supplies the address to a pointer. This function
+ *       will fill it in
+ * @keybuf: Pointer to key data
+ * @keylen: Number of bytes in the key (must be 16, 24, 32)
+ * @twkbuf: Pointer to tweak data (must be 7 bytes long and cannot be NULL)
+ * @radix: The radix of the plain/cipher text data
+ *
+ * @return 0 on success or a negative error number on failure
+ */
 int ff3_1_ctx_create(struct ff3_1_ctx ** const ctx,
                      const uint8_t * const keybuf, const size_t keylen,
                      const uint8_t * const twkbuf,
                      const unsigned int radix);
 
+/*
+ * Encrypt data using the FF1 algorithm
+ *
+ * Encryption is a "one-shot" operation. The entire plain text must
+ * be passed in, and the entire cipher text is returned. The cipher
+ * text uses the same "alphabet" as the plain text and will be the
+ * same length as the plain text.
+ *
+ * @ctx: The pointer returned by the create function
+ * @Y: A pointer to the location to output the cipher text. This space
+ *     is allocated by the caller and must be at least as long as the
+ *     plain text. The parameter must include space for a nul-terminator.
+ * @X: A pointer to the location of the plain text. The plain text must
+ *     be nul-terminated
+ * @T: A pointer to the "tweak" parameter. This is a sequence of bytes
+ *     and may be NULL. In the event that the tweak is NULL, the tweak
+ *     supplied to the create function will be used.
+ *
+ * @return 0 on success or a negative error number on failure
+ */
 int ff3_1_encrypt(struct ff3_1_ctx * const ctx,
                   char * const Y,
                   const char * const X, const uint8_t * const T);
+
+/*
+ * Decrypt data using the FF3-1 algorithm
+ *
+ * Decryption is a "one-shot" operation. The entire cipher text must
+ * be passed in, and the entire plain text is returned.
+ *
+ * @ctx: The pointer returned by the create function
+ * @Y: A pointer to the location to output the plain text. This space
+ *     is allocated by the caller and must be at least as long as the
+ *     cipher text. The parameter must include space for a nul-terminator.
+ * @X: A pointer to the location of the cipher text. The cipher text must
+ *     be nul-terminated
+ * @T: A pointer to the "tweak" parameter. This is a sequence of bytes
+ *     and may be NULL. In the event that the tweak is NULL, the tweak
+ *     supplied to the create function will be used. The tweak must be
+ *     the same as that used to perform the encryption
+ *
+ * @return 0 on success or a negative error number on failure
+ */
 int ff3_1_decrypt(struct ff3_1_ctx * const ctx,
                   char * const Y,
                   const char * const X, const uint8_t * const T);
 
+/*
+ * Destroy the context structure associated with the FF3-1 algorithm
+ *
+ * @ctx: The pointer returned by the create function
+ */
 void ff3_1_ctx_destroy(struct ff3_1_ctx * const ctx);
 
 __END_DECLS
