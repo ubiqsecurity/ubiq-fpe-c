5166e3e957d8527bf0e1336683b506481120c751
John Tyner <jtyner@ubiqsecurity.com>
2021-07-09 11:35:35 -0400

    add unit tests for radix conversion

---
+++ src/test/radix.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <ubiq/fpe/internal/radix.h>
+
+static
+void radix_test(const char * const input, const char * const ialpha,
+                const char * const oalpha, const char * const expect)
+{
+    char output[64];
+    int res;
+
+    res = radix_convert(output, oalpha, input, ialpha);
+    EXPECT_EQ(res, 0);
+    EXPECT_EQ(std::string(output), expect);
+}
+
+TEST(radix, dec2hex)
+{
+    radix_test("100", "0123456789", "0123456789ABCDEF", "64");
+}
+
+TEST(radix, oct2hex)
+{
+    radix_test("100", "01234567", "0123456789ABCDEF", "40");
+}
